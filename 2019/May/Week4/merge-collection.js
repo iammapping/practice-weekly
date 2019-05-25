b144a20fc92d2b6f5dd7bcb2af86df4d895fddc4
guyu <1029067771@qq.com>
2019-05-25 18:34:30 +0800

    2019 may week4

--- 2019/May/Week4/merge-collection.js
+++ 2019/May/Week4/merge-collection.js
@@ -1,3 +1,51 @@
+function getMergedObject(keys, baseObj, distObject) {
+
+  if (Array.isArray(keys)) {
+    let isFit = true;
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < keys.length; i++) {
+      if (distObject[i] !== baseObj[i]) {
+        isFit = false;
+        break;
+      }
+    }
+    if (isFit) {
+      return { ...baseObj, ...distObject }
+    }
+  } else if (typeof keys === 'string') {
+    if (distObject[keys] === baseObj[keys]) {
+      return { ...baseObj, ...distObject }
+    }
+  } else if (typeof keys === 'function') {
+    const val = keys(baseObj)
+    if (val && keys(distObject) === val) {
+      return { ...baseObj, ...distObject }
+    }
+  }
+
+  return baseObj
+};
+
+function mergeObject(retArr, keys, baseObj, restCollection) {
+  let cloneBaseObj = { ...baseObj }
+
+  restCollection.forEach(distCollection => {
+    if (Array.isArray(distCollection)) {
+      distCollection.forEach(distObject => {
+        cloneBaseObj = getMergedObject(keys, cloneBaseObj, distObject)
+      })
+
+    } else if (Object.prototype.toString.call(distCollection) === '[object Object]') {
+      Object.values(distCollection).forEach(distObject => {
+        cloneBaseObj = getMergedObject(keys, cloneBaseObj, distObject)
+      })
+    }
+  })
+
+  retArr.push(cloneBaseObj)
+};
+
 /**
  * 按指定键合并多个集合
  *
@@ -33,5 +81,17 @@
  * mergeCollection((o) => o.dim1, col1, col2, col3);
  */
 module.exports = function mergeCollection(keys, baseCollection, ...restCollection) {
+  const retArr = [];
 
+  if (Array.isArray(baseCollection)) {
+    baseCollection.forEach(baseObj => {
+      mergeObject(retArr, keys, baseObj, restCollection)
+    })
+  } else if (Object.prototype.toString.call(baseCollection) === '[object Object]') {
+    Object.values(baseCollection).forEach(baseObj => {
+      mergeObject(retArr, keys, baseObj, restCollection)
+    })
+  }
+
+  return retArr
 };
