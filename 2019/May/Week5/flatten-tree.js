646cc9a2df950e3b7f1b5e31124dd400827c647d
wjx <1723212606@qq.com>
2019-06-02 21:08:14 +0800

    2019.06.02-week5-wjx

--- 2019/May/Week5/flatten-tree.js
+++ 2019/May/Week5/flatten-tree.js
@@ -35,5 +35,61 @@
  * ]
  */
 module.exports = function flatten2tree(flattenArr, id = 'id', pid = 'pid', level = 'level', children = 'children') {
+  if (!Array.isArray(flattenArr)) {
+    return [];
+  }
 
+  let maxLevel = 0;
+  let minLevel = 0;
+
+  flattenArr.forEach(item => {
+    if (maxLevel === 0 || item[level] > maxLevel) {
+      maxLevel = item[level];
+    }
+
+    if (minLevel === 0 || item[level] < minLevel) {
+      minLevel = item[level];
+    }
+  });
+
+  let nowLevel = maxLevel;
+  let currentLevelNodes = [];
+  let lastLevelNodes = [];
+
+  function buildData() {
+    lastLevelNodes = currentLevelNodes;
+    currentLevelNodes = [];
+
+    flattenArr.forEach(it => {
+      const item = it;
+
+      if (item[level] !== nowLevel) return;
+
+      const node = item[id];
+      const pNode = item[pid];
+
+      if (lastLevelNodes[node]) {
+        item[children] = lastLevelNodes[node];
+      }
+
+      if (!currentLevelNodes[pNode]) {
+        currentLevelNodes[pNode] = [];
+      }
+
+      currentLevelNodes[pNode].push(item);
+    });
+
+    nowLevel -= 1;
+  }
+
+  while (nowLevel > (minLevel - 1)) { // 2 > 0
+    buildData();
+  }
+
+  let tree = [];
+  currentLevelNodes.forEach(nodes => {
+    tree = tree.concat(nodes);
+  });
+
+  return tree;
 }
