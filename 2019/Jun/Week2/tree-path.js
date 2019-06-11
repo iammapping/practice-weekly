215a311cba5baa53b87668793f0d8da7fe602fc5
raojinlin <1239015423@qq.com>
2019-06-12 00:42:26 +0800

    jun-week2

--- 2019/Jun/Week2/tree-path.js
+++ 2019/Jun/Week2/tree-path.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 const LOOKUPTYPE = {
   // 只搜索叶子节点，即没有 children 的节点
   ONLY_LEAF: 1,
@@ -49,7 +50,113 @@ const LOOKUPTYPE = {
  * ]
  */
 module.exports = function lookupTreePath(tree, predicate, lookupType = LOOKUPTYPE.ONLY_LEAF) {
+  function isMatch(node, pred) {
+    if (typeof pred === "function") {
+      return pred(node);
+    }
 
-}
+    if (typeof pred === "object") {
+       let match = false;
+
+       /** eslint no-restricted-syntax: "error" */
+       for (const k in pred) {
+         if (Object.prototype.hasOwnProperty.call(pred, k)) {
+           if (typeof pred[k] === "function") {
+             match = pred[k](node[k]);
+           } else {
+             match = node[k] === pred[k];
+           }
+         }
+       }
+
+       return match;
+    }
+
+    return false;
+  }
+
+  function getParent(node, path) {
+    let parentIndex = -1;
+    const parent = path.find((sp, si) => {
+      const last = sp[sp.length - 1] || [];
+      if (last && last.id === node.pid) {
+        parentIndex = si;
+        return true;
+      }
+      return false;
+    });
+
+    return [parent, parentIndex];
+  }
+
+  function getPath(nodes, pred) {
+    const stack = [...nodes];
+    const path = [];
+    const pathIndex = [];
+
+    while (stack.length) {
+      const node = stack.pop();
+      const { pid, id, level, name } = node;
+      const newNode = { pid, id, level, name };
+      const [parent, parentIndex] = getParent(newNode, path);
+
+      if (node.children) {
+        for (let i = 0; i < node.children.length; i++) {
+          stack.push(node.children[i]);
+
+          let index = -1;
+          const match = isMatch(newNode, pred);
+          if (parentIndex !== -1) {
+            if (i === 0) {
+              path[parentIndex] = [...parent, newNode];
+            } else {
+              path.push([...parent, newNode]);
+            }
+
+            index = parentIndex;
+          } else {
+            path.push([newNode]);
+            index = path.length -1;
+          }
+
+          if (lookupType === LOOKUPTYPE.ALWAYS_CHILDREN && match) {
+            pathIndex.push(index);
+          }
+
+          if (lookupType === LOOKUPTYPE.WITHOUT_CHILDREN && match) {
+            pathIndex.push(index);
+            break;
+          }
+        }
+
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
+
+      let index = -1;
+      if (parent && lookupType !== LOOKUPTYPE.WITHOUT_CHILDREN) {
+        parent.push(newNode);
+        index = parentIndex;
+      } else {
+        path.push([newNode]);
+        index = path.length -1;
+      }
+
+      if (lookupType === LOOKUPTYPE.ONLY_LEAF && isMatch(newNode, pred)) {
+        pathIndex.push(index);
+      }
+    }
+
+    const result = [];
+    while (pathIndex.length) {
+      result.push(path[pathIndex.pop()]);
+    }
+
+    return result;
+  }
+
+  return getPath(tree, predicate)
+};
 
 module.exports.LOOKUPTYPE = LOOKUPTYPE;
