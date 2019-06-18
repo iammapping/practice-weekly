7fac7d5978b243a4e6dde37e509a6d12a572df0f
superman0922 <twxlj88@163.com>
2019-06-19 07:48:04 +0800

    Chaoren call-chain

--- 2019/Jun/Week3/call-chain.js
+++ 2019/Jun/Week3/call-chain.js
@@ -13,7 +13,28 @@
  * args: [1, 2, 3]
  */
 function CallChain(executor) {
+    let props = [];
+    let hasCall = false;
 
+    function proxy(exctor) {
+        return new Proxy(exctor || {}, {
+            get: (target, prop) => {
+                if (hasCall) {
+                    props = [];
+                    hasCall = false;
+                }
+
+                props.push(prop);
+                return proxy(target);
+            },
+            apply: async (target, ctx, args) => {
+                hasCall = true;
+                await target(props.join('.'), args);
+            }
+        });
+    }
+    
+    return proxy(executor);
 }
 
 module.exports = CallChain;
