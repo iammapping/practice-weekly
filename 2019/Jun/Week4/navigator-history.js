61d3c45644f94629dc2cd939df0968557cdc4a6e
iammapping <meibing_240@126.com>
2019-06-19 14:03:49 +0800

    add 2019 jun week4 practice

---
+++ 2019/Jun/Week4/navigator-history.js
@@ -0,0 +1,70 @@
+/**
+ * 导航历史，实现类似浏览器的前进后退
+ */
+class NavigatorHistory {
+  constructor() {
+
+  }
+
+  /**
+   * 导航到某地址
+   *
+   * @param {any} url
+   * @memberof NavigatorHistory
+   */
+  navigate(url) {
+
+  }
+
+  /**
+   * 后退
+   *
+   * @returns {any} url
+   * @memberof NavigatorHistory
+   */
+  back() {
+
+  }
+
+  /**
+   * 判断是否可以后退
+   *
+   * @returns {boolean}
+   * @memberof NavigatorHistory
+   */
+  isBackAvailable() {
+
+  }
+
+  /**
+   * 前进
+   *
+   * @returns {any} url
+   * @memberof NavigatorHistory
+   */
+  forward() {
+
+  }
+
+  /**
+   * 判断是否可以前进
+   *
+   * @returns {boolean}
+   * @memberof NavigatorHistory
+   */
+  isForwardAvailable() {
+
+  }
+
+  /**
+   * 当前
+   *
+   * @returns {any} url
+   * @memberof NavigatorHistory
+   */
+  current() {
+
+  }
+}
+
+module.exports = NavigatorHistory;
