4747240a2078db07d2a47424c94e3af07587f8e8
guyu <1029067771@qq.com>
2019-07-03 09:47:49 +0800

    Guyu:2019 Jun Week5

--- 2019/Jun/Week5/evaluate-expression.js
+++ 2019/Jun/Week5/evaluate-expression.js
@@ -1,3 +1,91 @@
+function evaluateSingle(eva) {
+  const reg = /([0-9]+)/
+  const arr = reg.exec(eva)
+  if (!arr) {
+    throw new Error('invalid expression')
+  }
+
+  const index = eva.indexOf(arr[0]) + arr[0].length
+  if (index >= eva.length) {
+    return parseFloat(eva)
+  }
+  const left = parseFloat(eva.substring(0, index))
+  const mid = eva.substring(index, index + 1)
+  const right = parseFloat(eva.substring(index + 1))
+
+  if (mid === '*')
+    return left * right
+
+  if (mid === '/')
+    return left / right
+
+  if (mid === '+')
+    return left + right
+
+  if (mid === '*')
+    return left - right
+
+  throw new Error('invalid expression')
+}
+
+function evaluateLower(eva) {
+  // 按照顺序计算
+  const reg = /(\+|-)*[0-9]+(\*|\/|\+|-)+(\+|-)*[0-9]+/g
+  const arr = reg.exec(eva)
+  if (!arr) {
+    return evaluateSingle(eva)
+  }
+
+  const singleEva = arr[0]
+  const split = eva.split(singleEva)
+  const right = split[1]
+
+  if (right === '') {
+    return evaluateSingle(singleEva)
+  }
+
+  return evaluateLower(evaluateSingle(singleEva) + right)
+}
+
+function evaluateSenior(eva) {
+  const reg = /(\+|-)*[0-9]+(\*|\/)+(\+|-)*[0-9]+/g
+  const arr = reg.exec(eva)
+
+  if(arr) {
+
+    let mid = arr[0]
+    const split = eva.split(mid)
+    let left = split[0]
+    const right = split[1]
+
+    if (left !== '' && !/(\+|-)&/.exec(left)) {
+      left += mid.substring(0, 1)
+      mid = mid.substring(1)
+    }
+
+    return evaluateSenior(left + evaluateLower(mid) + right)
+  }
+
+  return evaluateLower(eva)
+}
+
+function evaluateBracket(eva) {
+  const begin = eva.indexOf('(')
+
+  if (begin > -1) {
+    const end = eva.lastIndexOf(')')
+    if (end === -1)
+      throw new Error('invalid expression')
+
+    return evaluateSenior(eva.substring(0, begin) 
+      + evaluateBracket(eva.substring(begin + 1, end)) 
+      + (end + 1 >= eva.length ? '' : eva.substring(end + 1)))
+  }
+
+  // 无括号
+  return evaluateSenior(eva)
+}
+
 /**
  * 计算四则运算表达式的结果
  *
@@ -8,5 +96,7 @@
  * 7
  */
 module.exports = function evaluate(expression) {
+  const noSpace = expression.replace(/ /g, '')
 
+  return evaluateBracket(noSpace)
 };
