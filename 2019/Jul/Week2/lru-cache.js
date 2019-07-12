d2d212481bdeb7e3016ab5b7df430446a24a1956
iammapping <meibing_240@126.com>
2019-07-12 22:36:30 +0800

    add 2019 jul week2 practice

---
+++ 2019/Jul/Week2/lru-cache.js
@@ -0,0 +1,65 @@
+/**
+ * 实现一个 LRU Cache
+ */
+module.exports = class LruCache {
+  /**
+   *
+   * @param {number} max 最大容量
+   */
+  constructor(max) {
+
+  }
+
+  /**
+   * 已存储的数量
+   *
+   * @readonly
+   */
+  get length() {
+
+  }
+
+  /**
+   * 获取某个 key 对应的值
+   *
+   * @param {*} key
+   */
+  get(key) {
+
+  }
+
+  /**
+   * 设置键值对
+   *
+   * @param {any} key
+   * @param {any} val
+   */
+  set(key, val) {
+
+  }
+
+  /**
+   * 删除某个 key
+   *
+   * @param {any} key
+   */
+  del(key) {
+
+  }
+
+  /**
+   * 判断某个 key 是否存在
+   *
+   * @param {any} key
+   */
+  has(key) {
+
+  }
+
+  /**
+   * 清空所有的内容
+   */
+  reset() {
+
+  }
+}
