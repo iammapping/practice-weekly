ad5e6c6a7b84700bb8b480c834ff5efe106d8fb3
kongxiangjin <1351882069@qq.com>
2019-07-14 23:19:50 +0800

    lru

--- 2019/Jul/Week2/lru-cache.js
+++ 2019/Jul/Week2/lru-cache.js
@@ -1,13 +1,26 @@
 /**
  * 实现一个 LRU Cache
  */
+
+// function Node() {
+//   this.key = null;
+//   this.value = null;
+//   //this.obj = [];
+//   this.previous = null;
+//   this.next = null;
+// }
+function Node() {
+  this.key = null;
+  this.value = null;
+}
 module.exports = class LruCache {
   /**
    *
    * @param {number} max 最大容量
    */
   constructor(max) {
-
+    this.maxLength = max;
+    this.lru = [];
   }
 
   /**
@@ -16,7 +29,7 @@ module.exports = class LruCache {
    * @readonly
    */
   get length() {
-
+    return this.lru.length
   }
 
   /**
@@ -25,7 +38,14 @@ module.exports = class LruCache {
    * @param {*} key
    */
   get(key) {
-
+    for (let i=0; i<this.lru.length; i++) {
+      if (key === this.lru[i].key) {
+        const node = this.lru.splice(i,1);
+        this.lru.unshift({key:node[0].key,value:node[0].value});
+        return node[0].value;
+      }
+    }
+    return undefined;
   }
 
   /**
@@ -35,7 +55,24 @@ module.exports = class LruCache {
    * @param {any} val
    */
   set(key, val) {
+    // let element = this.node;
+    // if(this.has(key)) {
+    //   element.key = key;
+    //   element.value = val;
+    //
+    // }
+    // this.lru.push(element);
 
+    if(!this.has(key)) {
+      const element = {
+        key,
+        value: val
+      }
+      if(this.lru.length === this.maxLength ) {
+        this.del(key);
+      }
+      this.lru.unshift(element);
+    }
   }
 
   /**
@@ -44,7 +81,12 @@ module.exports = class LruCache {
    * @param {any} key
    */
   del(key) {
-
+    for (let i=0; i<this.lru.length; i++) {
+      if (key === this.lru[i].key) {
+       return this.lru.splice(i, 1);
+      }
+    }
+    return  this.lru.pop();
   }
 
   /**
@@ -53,13 +95,18 @@ module.exports = class LruCache {
    * @param {any} key
    */
   has(key) {
-
+    for (let i=0; i<this.lru.length; i++) {
+      if (key === this.lru[i].key) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /**
    * 清空所有的内容
    */
   reset() {
-
+    this.lru.splice(0, this.lru.length)
   }
 }
