29094a578eb2356a98347ec57b702eacaa03df95
guyu <1029067771@qq.com>
2019-07-20 16:18:01 +0800

    Guyu: 2019 Jul Week3

--- 2019/Jul/Week3/email-suggest.js
+++ 2019/Jul/Week3/email-suggest.js
@@ -1,9 +1,47 @@
+/* eslint-disable class-methods-use-this */
+/* esllet-disable class-methods-use-this */
+
 /**
  * 邮箱正确后缀建议
  */
 module.exports = class EmailSuggestion {
   constructor(suggestedSuffixes = []) {
     this.suggestedSuffixes = suggestedSuffixes;
+    this.tolerateDistance = 3
+  }
+
+  lev(a, b) {
+    const alen = a.length
+    const blen = b.length
+
+    if(Math.min(alen, blen) === 0) {
+      return Math.max(alen, blen)
+    }
+    
+    const arr = [];
+    for (let i = 0; i <= alen; i++) {
+      if(!arr[i]) {
+        arr[i] = []
+      }
+      arr[i][0] = i;
+    }
+    for (let j = 0; j <= blen; j++) {
+      arr[0][j] = j;
+    }
+
+    for (let i = 0; i < alen; i++) {
+      for (let j = 0; j < blen; j++) {
+        if (a.charAt(i) === b.charAt(j)) {
+          arr[i + 1][j + 1] = arr[i][j];
+        } else {
+          const leftInsert = arr[i][j + 1] + 1
+          const aboveDelete = arr[i + 1][j] + 1
+          const replace = arr[i][j] + 1
+          arr[i + 1][j + 1] = Math.min(leftInsert, aboveDelete, replace);
+        }
+      }
+    }
+    return arr[alen][blen];
   }
 
   /**
@@ -13,6 +51,18 @@ module.exports = class EmailSuggestion {
    * @returns {array} 所有建议的邮箱
    */
   suggest(email) {
+    const index = email.indexOf('@')
+    const preffix = email.substring(0, index + 1)
+    const suffix = email.substring(index + 1)
 
+    const map = {}
+    this.suggestedSuffixes.forEach(item => {
+      if (Math.abs(item.length - suffix.length) <= this.tolerateDistance) {
+        const lev = this.lev(item, suffix)
+        map[preffix + item] = lev
+      }
+    })
+
+    return Object.keys(map).sort((a, b) => map[a] - map[b])
   }
 }
