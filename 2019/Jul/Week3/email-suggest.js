dcf69f2cf614ae75708bd5833af8ec4a39690e69
raojinlin <1239015423@qq.com>
2019-07-21 19:31:06 +0800

    raojinlin: 2019-jul-week3

--- 2019/Jul/Week3/email-suggest.js
+++ 2019/Jul/Week3/email-suggest.js
@@ -1,9 +1,74 @@
 /**
+ * minimum value
+ *
+ * @param a {number}
+ * @param b {number}
+ * @param c {number}
+ * @return {number}
+ */
+function min(a, b, c) {
+  let m = a;
+
+  if (b < m) {
+    m = b
+  }
+
+  if (c < m) {
+    m = c;
+  }
+
+  return m;
+}
+
+/**
+ * Levenshtein Distance
+ *
+ * @param src {string}
+ * @param dest {string}
+ * @return {number}
+ */
+function distance(src, dest) {
+  const m = src.length + 1;
+  const n = dest.length + 1;
+  const matrix = [];
+
+  // i == 0
+  for (let i = 0; i < m; i++) {
+    if (!Array.isArray(matrix[i])) {
+      matrix.push([]);
+    }
+
+    matrix[i][0] = i;
+  }
+
+  // j == 0
+  for (let j = 0; j < n; j++) {
+    matrix[0][j] = j;
+  }
+
+  // j >= 1 & i >= 1
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      const cost = src[i - 1] === dest[j - 1] ? 0 : 1;
+
+      matrix[i][j] = min(
+        matrix[i - 1][j] + 1,
+        matrix[i][j - 1] + 1,
+        matrix[i - 1][j - 1] + cost
+      )
+    }
+  }
+
+  return matrix[m - 1][n - 1];
+}
+
+/**
  * 邮箱正确后缀建议
  */
 module.exports = class EmailSuggestion {
   constructor(suggestedSuffixes = []) {
     this.suggestedSuffixes = suggestedSuffixes;
+    this.maxDistance = 2;
   }
 
   /**
@@ -13,6 +78,17 @@ module.exports = class EmailSuggestion {
    * @returns {array} 所有建议的邮箱
    */
   suggest(email) {
+    const username = email.slice(0, email.indexOf('@'));
+    const address = email.slice(email.indexOf('@') + 1);
+    const result = [];
 
+    // eslint-disable-next-line array-callback-return
+    this.suggestedSuffixes.forEach(suggested => {
+      if (distance(address, suggested) <= this.maxDistance) {
+        result.push(`${username}@${suggested}`);
+      }
+    });
+
+    return result;
   }
-}
+};
