5e45728d247e6fa9e0dee30b548ecebc53f0ac97
kong0827 <45532757+kong0827@users.noreply.github.com>
2019-07-22 12:51:42 +0800

    kongxiangjin

--- 2019/Jul/Week3/email-suggest.js
+++ 2019/Jul/Week3/email-suggest.js
@@ -6,6 +6,38 @@ module.exports = class EmailSuggestion {
     this.suggestedSuffixes = suggestedSuffixes;
   }
 
+
+  static levenshtein (a, b) {
+
+    const matrix = [];
+
+    let i;
+    for(i = 0; i <= b.length; i++){
+      matrix[i] = [i];
+    }
+
+    let j;
+    for(j = 0; j <= a.length; j++){
+      matrix[0][j] = j;
+    }
+
+    for(i = 1; i <= b.length; i++){
+      for(j = 1; j <= a.length; j++){
+        if(b.charAt(i-1) === a.charAt(j-1)){
+          matrix[i][j] = matrix[i-1][j-1];
+        } else {
+          matrix[i][j] = Math.min(matrix[i-1][j-1] + 1,
+            Math.min(matrix[i][j-1] + 1,
+              matrix[i-1][j] + 1));
+        }
+      }
+    }
+
+    // console.log(matrix)
+    return matrix[b.length][a.length];
+
+  }
+;
   /**
    * 根据输入的邮箱，建议可能正确的邮箱
    * @param {string} email
@@ -13,6 +45,17 @@ module.exports = class EmailSuggestion {
    * @returns {array} 所有建议的邮箱
    */
   suggest(email) {
+    // const preffix = email.split('@')[0];
+    const emailType = email.split('@')[1];
+    const emailArray = [];
+
+    this.suggestedSuffixes.forEach(suggestItem => {
+      const index = EmailSuggestion.levenshtein(suggestItem, emailType);
+      if (index < 3) {
+        emailArray.push(`${email.slice(0,email.indexOf('@'))}@${suggestItem}`);
+      }
+    });
+    return emailArray;
 
   }
-}
+};
