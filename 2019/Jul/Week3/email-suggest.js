3608554896bd9faeca497a6de220a6b553be688a
superman0922 <twxlj88@163.com>
2019-07-23 22:40:55 +0800

    Chaoren email-suggest

--- 2019/Jul/Week3/email-suggest.js
+++ 2019/Jul/Week3/email-suggest.js
@@ -1,3 +1,19 @@
+function initTable(sugSuf, suff) {
+  const table = [];
+  for (let i = 0; i <= sugSuf.length; i++) {
+    const row = [];
+    for (let j = i; j <= suff.length + i; j++) {
+      if (i === 0 || i === j) { 
+        row.push(j);
+      } else {
+        row.push(0);
+      }
+    }
+    table.push(row);
+  }
+  return table;
+}
+
 /**
  * 邮箱正确后缀建议
  */
@@ -13,6 +29,21 @@ module.exports = class EmailSuggestion {
    * @returns {array} 所有建议的邮箱
    */
   suggest(email) {
+    const suff = email.split('@')[1];
 
+    const degrees = this.suggestedSuffixes.map(sugSuf => {
+      const table = initTable(sugSuf, suff);
+      for (let i = 1; i < table.length; i++) {
+        for (let j = 1; j < table[i].length; j++) {
+          table[i][j] = Math.min(table[i - 1][j] + 1, table[i][j - 1] + 1, table[i - 1][j - 1] + (sugSuf[i - 1] === suff[j - 1] ? 0 : 1));
+        }
+      }
+
+      const matchDegree = 1 - table[sugSuf.length][suff.length] / Math.max(sugSuf.length, suff.length);
+      return {'degree' : matchDegree, 'sugSuf' : matchDegree === 0 ? undefined : sugSuf};
+    });
+
+    const orderDegrees = degrees.sort((degree1, degree2) => degree2.degree > degree1.degree);
+    return orderDegrees.map(orderDegree => `${email.split('@')[0]}@${orderDegree.sugSuf}`);
   }
 }
