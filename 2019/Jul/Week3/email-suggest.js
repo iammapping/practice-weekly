b3ba5ab8ff964f0062a75811c1102aac5d2f40e9
superman0922 <twxlj88@163.com>
2019-07-24 08:05:50 +0800

    Chaoren email-suggest

--- 2019/Jul/Week3/email-suggest.js
+++ 2019/Jul/Week3/email-suggest.js
@@ -14,12 +14,23 @@ function initTable(sugSuf, suff) {
   return table;
 }
 
+function getMatchDegree(str1, str2) {
+  const table = initTable(str1, str2);
+  for (let i = 1; i < table.length; i++) {
+    for (let j = 1; j < table[i].length; j++) {
+      table[i][j] = Math.min(table[i - 1][j] + 1, table[i][j - 1] + 1, table[i - 1][j - 1] + (str1[i - 1] === str2[j - 1] ? 0 : 1));
+    }
+  }
+  return 1 - table[str1.length][str2.length] / Math.max(str1.length, str2.length);
+}
+
 /**
  * 邮箱正确后缀建议
  */
 module.exports = class EmailSuggestion {
   constructor(suggestedSuffixes = []) {
     this.suggestedSuffixes = suggestedSuffixes;
+    this.minMatchDegree = 0.5;
   }
 
   /**
@@ -29,21 +40,23 @@ module.exports = class EmailSuggestion {
    * @returns {array} 所有建议的邮箱
    */
   suggest(email) {
-    const suff = email.split('@')[1];
+    const [pre, suff] = email.split('@');
 
     const degrees = this.suggestedSuffixes.map(sugSuf => {
-      const table = initTable(sugSuf, suff);
-      for (let i = 1; i < table.length; i++) {
-        for (let j = 1; j < table[i].length; j++) {
-          table[i][j] = Math.min(table[i - 1][j] + 1, table[i][j - 1] + 1, table[i - 1][j - 1] + (sugSuf[i - 1] === suff[j - 1] ? 0 : 1));
-        }
-      }
-
-      const matchDegree = 1 - table[sugSuf.length][suff.length] / Math.max(sugSuf.length, suff.length);
+      const matchDegree = getMatchDegree(sugSuf, suff);
       return {'degree' : matchDegree, 'sugSuf' : matchDegree === 0 ? undefined : sugSuf};
     });
 
-    const orderDegrees = degrees.sort((degree1, degree2) => degree2.degree > degree1.degree);
-    return orderDegrees.map(orderDegree => `${email.split('@')[0]}@${orderDegree.sugSuf}`);
+    const orderDegrees = degrees.filter(degree => degree.degree >= this.minMatchDegree).sort((degree1, degree2) => {
+      if (degree2.degree > degree1.degree) {
+        return 1;
+      }  
+      if (degree2.degree < degree1.degree) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return orderDegrees.map(orderDegree => `${pre}@${orderDegree.sugSuf}`);
   }
 }
