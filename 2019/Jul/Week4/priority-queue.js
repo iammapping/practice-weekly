62f2a54f39054690275b71e98904e1e44acccb60
wangteng <wt@datacaciques.com>
2019-07-27 22:42:45 +0800

    jul-week4

--- 2019/Jul/Week4/priority-queue.js
+++ 2019/Jul/Week4/priority-queue.js
@@ -9,6 +9,8 @@ module.exports = class PriorityQueue {
       // 队列元素的优先级比对方法
       comparator: (a, b) => b - a,
     }, options);
+
+    this.queueArr = this.options.initValues.sort(this.options.comparator);
   }
 
   /**
@@ -16,7 +18,7 @@ module.exports = class PriorityQueue {
    * @readonly
    */
   get length() {
-
+    return this.queueArr.length;
   }
 
   /**
@@ -24,7 +26,19 @@ module.exports = class PriorityQueue {
    * @param {any} value
    */
   queue(value) {
+    const self = this;
+    let queueIndex = 0;
 
+    for(let i = 0; i < this.queueArr.length; i += 1) {
+      const queueValue = this.queueArr[i];
+      if(self.options.comparator(queueValue, value) <= 0) {
+        queueIndex = i + 1;
+      } else {
+        break;
+      }
+    }
+
+    self.queueArr.splice(queueIndex, 0, value);
   }
 
   /**
@@ -32,20 +46,20 @@ module.exports = class PriorityQueue {
    * @return {any}
    */
   dequeue() {
-;
+    return this.queueArr.pop();
   }
 
   /**
    * 获取队列最优先的值
    */
   peek() {
-
+    return this.queueArr[this.queueArr.length - 1];
   }
 
   /**
    * 清空队列
    */
   clear() {
-
+    this.queueArr = [];
   }
 };
