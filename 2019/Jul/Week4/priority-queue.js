c6a5e15e008545a4c1ff87f5a1bc56bad515213d
lg <kwtdgl13@outlook.com>
2019-07-30 18:27:38 +0800

    save

--- 2019/Jul/Week4/priority-queue.js
+++ 2019/Jul/Week4/priority-queue.js
@@ -1,5 +1,43 @@
+class Node {
+  constructor(value, left, right) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+function treeAddNode(root, node, comparator){
+  if(comparator(root.value, node.value) > 0){
+    if(root.right === null) {
+      root.right = node;
+    }else {
+      treeAddNode(root.right, node, comparator);
+    }
+  }else if(root.left === null) {
+    root.left = node;
+  }else {
+    treeAddNode(root.left, node, comparator);
+  }
+}
+
+function getPriorityNode(root) {
+  return root.left === null ? root : getPriorityNode(root.left);
+}
+
+function outPriorityNode(preNode, currentNode) {
+  if(currentNode.left !== null) {
+    return outPriorityNode(currentNode, currentNode.left);
+  }
+  if(currentNode.right !== null) {
+    preNode.left = currentNode.right;
+    return currentNode;
+  }
+  preNode.left = null;
+  return currentNode;
+}
+
 /**
- * 实现一个优先级队列
+ * 实现一个优先级队列, 二叉树
  */
 module.exports = class PriorityQueue {
   constructor(options) {
@@ -9,6 +47,30 @@ module.exports = class PriorityQueue {
       // 队列元素的优先级比对方法
       comparator: (a, b) => b - a,
     }, options);
+
+    this.treeBaseNode = null;
+    this.options.initValues.map(e => {
+      const node = new Node(e, null, null);
+      this.addNode(node);
+      return true;
+    });
+    this.currentLength = this.options.initValues.length;
+  }
+
+  addNode(node){
+    if(this.treeBaseNode === null) {
+      this.treeBaseNode = node;
+    }else {
+      treeAddNode(this.treeBaseNode, node, this.options.comparator);
+    }
+  }
+
+  outNode(){
+    if(this.treeBaseNode.left == null) {
+      this.treeBaseNode = this.treeBaseNode.right;
+      return this.treeBaseNode;
+    }
+    return outPriorityNode(this.treeBaseNode, this.treeBaseNode.left);
   }
 
   /**
@@ -16,7 +78,7 @@ module.exports = class PriorityQueue {
    * @readonly
    */
   get length() {
-
+    return this.currentLength;
   }
 
   /**
@@ -24,7 +86,8 @@ module.exports = class PriorityQueue {
    * @param {any} value
    */
   queue(value) {
-
+    this.addNode(new Node(value, null, null));
+    this.currentLength++;
   }
 
   /**
@@ -32,20 +95,25 @@ module.exports = class PriorityQueue {
    * @return {any}
    */
   dequeue() {
-;
+    if(this.currentLength <= 0) {
+      return undefined;
+    }
+    this.currentLength--;
+    return this.outNode().value;
   }
 
   /**
    * 获取队列最优先的值
    */
   peek() {
-
+    return this.currentLength <= 0 ? undefined : getPriorityNode(this.treeBaseNode).value;
   }
 
   /**
    * 清空队列
    */
   clear() {
-
+    this.treeBaseNode = null;
+    this.currentLength = 0;
   }
 };
