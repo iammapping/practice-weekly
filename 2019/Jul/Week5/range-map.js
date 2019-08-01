2ab7a40aa76fc7c1749906eb4a08b4140c1c5bb6
iammapping <meibing_240@126.com>
2019-08-01 14:04:18 +0800

    add 2019 jul week5 practice

---
+++ 2019/Jul/Week5/range-map.js
@@ -0,0 +1,38 @@
+/**
+ * 实现一个区间 Map
+ */
+module.exports = class RangeMap {
+  /**
+   * 添加区间与值的映射
+   * @param {array<number>} range 整型范围区间 [start, end]
+   * @param {any} value
+   */
+  add(range, value) {
+
+  }
+
+  /**
+   * 查找命中的区间
+   * @param {number} search
+   * @returns {array<number>}
+   */
+  findRange(search) {
+
+  }
+
+  /**
+   * 查找命中区间对应的值
+   * @param {number} search
+   * @returns {any}
+   */
+  findValue(search) {
+
+  }
+
+  /**
+   * 清空 Map
+   */
+  clear() {
+
+  }
+};
