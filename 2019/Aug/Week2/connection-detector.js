75096e7aeffadfed8f3e7eacae77fbb526b2d5b2
iammapping <meibing_240@126.com>
2019-08-07 10:18:35 +0800

    add 2019 aug week2 practice

---
+++ 2019/Aug/Week2/connection-detector.js
@@ -0,0 +1,32 @@
+/**
+ * 联通性检测
+ */
+module.exports = class ConnectionDetector {
+  constructor(options) {
+    this.options = Object.assign({
+      // 节点用于索引的属性，默认为节点本身
+      identify: o => o
+    }, options);
+  }
+
+  /**
+   * 每次添加的一批节点即表示这一批节点任意两个都联通
+   * @param  {...any} nodes
+   */
+  addConnectedNodes(...nodes) {
+    if (nodes.length < 2) {
+      throw new Error('You should provide at least two nodes each time.');
+    }
+
+
+  }
+
+  /**
+   * 判断两个节点是否联通
+   * @param {any} nodeA
+   * @param {any} nodeB
+   */
+  isConntectedTo(nodeA, nodeB) {
+
+  }
+}
