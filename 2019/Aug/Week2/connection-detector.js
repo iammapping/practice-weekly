5194fd3bfe4f9163ec10d9155758c842339e9d0d
it <2446426696@qq.com>
2019-08-10 12:59:58 +0800

    liuweitao:week2 connection-detector

--- 2019/Aug/Week2/connection-detector.js
+++ 2019/Aug/Week2/connection-detector.js
@@ -7,7 +7,9 @@ module.exports = class ConnectionDetector {
       // 节点用于索引的属性，默认为节点本身
       identify: o => o
     }, options);
+    this.connectionRelationMap = new Map();
   }
+
 
   /**
    * 每次添加的一批节点即表示这一批节点任意两个都联通
@@ -17,8 +19,33 @@ module.exports = class ConnectionDetector {
     if (nodes.length < 2) {
       throw new Error('You should provide at least two nodes each time.');
     }
+    // 使用第一个节点所在到组名作为其他节点所在到组名
+    const root = this.search(nodes[0]);
+    for(let i = 1; i < nodes.length; i++){
+      if( root !== this.search(nodes[i]) ){
+        this.connectionRelationMap.set(this.search(nodes[i]), root);
+      }
+    }
+  }
 
-
+  search(root) {
+    let sonIdentify = this.options.identify(root);
+    let rootIdentify = this.options.identify(root);
+    // 如果节点没有值，说明它还没有组，将自己到组名设为自己
+    if(!this.connectionRelationMap.has(rootIdentify)) {
+      this.connectionRelationMap.set(rootIdentify, rootIdentify);
+    }
+    // 找到该节点的所在到组
+    while(rootIdentify !== this.connectionRelationMap.get(rootIdentify)) {
+      rootIdentify = this.connectionRelationMap.get(rootIdentify);
+    }
+    // 重新调整
+    while(sonIdentify !== rootIdentify) {
+      const tmp = this.connectionRelationMap.get(sonIdentify);
+      this.connectionRelationMap.set(sonIdentify, rootIdentify);
+      sonIdentify = tmp;
+    }
+    return rootIdentify;
   }
 
   /**
@@ -27,6 +54,6 @@ module.exports = class ConnectionDetector {
    * @param {any} nodeB
    */
   isConntectedTo(nodeA, nodeB) {
-
+    return this.search(nodeA) === this.search(nodeB);
   }
 }
