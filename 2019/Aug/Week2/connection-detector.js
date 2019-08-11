ceefb7559ff1c754b1085615c9fce3f949b4285d
superman0922 <twxlj88@163.com>
2019-08-11 18:11:37 +0800

    Chaoren connection-detector

--- 2019/Aug/Week2/connection-detector.js
+++ 2019/Aug/Week2/connection-detector.js
@@ -7,6 +7,9 @@ module.exports = class ConnectionDetector {
       // 节点用于索引的属性，默认为节点本身
       identify: o => o
     }, options);
+
+    this.connMap = {};
+    this.unionMap = {};
   }
 
   /**
@@ -18,7 +21,35 @@ module.exports = class ConnectionDetector {
       throw new Error('You should provide at least two nodes each time.');
     }
 
+    let firstId = null;
 
+    nodes.forEach(node => {
+      const id = this.options.identify(node);
+
+      if (this.connMap[id] === undefined) {
+        this.connMap[id] = (firstId === null ? id : firstId);
+
+        if (this.unionMap[this.connMap[id]] === undefined) {
+          this.unionMap[this.connMap[id]] = [id];
+        } else {
+          this.unionMap[this.connMap[id]].push(id);
+        }
+      }
+
+      if (firstId === null) {
+        firstId = this.connMap[id];
+      }
+
+      if (this.connMap[id] !== firstId) {
+        this.unionMap[firstId].forEach(uid => {
+          this.connMap[uid] = this.connMap[id];
+          this.unionMap[this.connMap[id]].push(uid);
+        });
+
+        delete this.unionMap[firstId];
+        firstId = this.connMap[id];
+      }
+    });
   }
 
   /**
@@ -27,6 +58,8 @@ module.exports = class ConnectionDetector {
    * @param {any} nodeB
    */
   isConntectedTo(nodeA, nodeB) {
-
+    const idA = this.options.identify(nodeA);
+    const idB = this.options.identify(nodeB);
+    return this.connMap[idA] !== undefined && this.connMap[idB] !== undefined && this.connMap[idA] === this.connMap[idB];
   }
 }
