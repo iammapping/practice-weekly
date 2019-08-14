036fa3b0d509512d29ef772279775c5309f34eed
wangteng <wt@datacaciques.com>
2019-08-14 10:59:27 +0800

    week practice

--- 2019/Aug/Week2/connection-detector.js
+++ 2019/Aug/Week2/connection-detector.js
@@ -7,6 +7,8 @@ module.exports = class ConnectionDetector {
       // 节点用于索引的属性，默认为节点本身
       identify: o => o
     }, options);
+
+    this.treeObj = {};
   }
 
   /**
@@ -18,7 +20,24 @@ module.exports = class ConnectionDetector {
       throw new Error('You should provide at least two nodes each time.');
     }
 
+    const firstNodeKey = this.options.identify(nodes[0]);
+    const pathKey = this.treeObj[firstNodeKey] && this.treeObj[firstNodeKey].path || firstNodeKey;
 
+    let previousKey = null;
+    nodes.forEach(node => {
+      const key = this.options.identify(node);
+
+      if (previousKey) {
+        this.treeObj[previousKey].next = key;
+      }
+      previousKey = key;
+
+      if (!this.treeObj[key]) {
+        this.treeObj[key] = {path: pathKey};
+      } else if (this.treeObj[key].path !== pathKey) {
+        this.changeNodePath(key, pathKey);
+      }
+    })
   }
 
   /**
@@ -27,6 +46,20 @@ module.exports = class ConnectionDetector {
    * @param {any} nodeB
    */
   isConntectedTo(nodeA, nodeB) {
+    const result = this.treeObj[this.options.identify(nodeA)]
+      && this.treeObj[this.options.identify(nodeB)]
+      && (this.treeObj[this.options.identify(nodeA)].path === this.treeObj[this.options.identify(nodeB)].path);
 
+    return !!result;
   }
+
+  changeNodePath(currentKey, path) {
+    this.treeObj[currentKey].path = path;
+
+    const nextKey = this.treeObj[currentKey] && this.treeObj[currentKey].next;
+    if (nextKey) {
+      this.changeNodePath(nextKey, path);
+    }
+  }
+
 }
