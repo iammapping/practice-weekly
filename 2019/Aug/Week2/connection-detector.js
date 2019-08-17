31126b5f181d8e84afa70125b3a7ff695d65e7d4
Superman <twxlj88@163.com>
2019-08-17 13:07:08 +0800

    Chaoren connection-detector (#96)
    * Chaoren connection-detector
    
    * Chaoren connection-detector

--- 2019/Aug/Week2/connection-detector.js
+++ 2019/Aug/Week2/connection-detector.js
@@ -7,6 +7,9 @@ module.exports = class ConnectionDetector {
       // 节点用于索引的属性，默认为节点本身
       identify: o => o
     }, options);
+
+    this.connMap = {};
+    this.unionMap = {};
   }
 
   /**
@@ -18,7 +21,38 @@ module.exports = class ConnectionDetector {
       throw new Error('You should provide at least two nodes each time.');
     }
 
+    let firstId = null;
 
+    nodes.forEach(node => {
+      const id = this.options.identify(node);
+
+      if (this.connMap[id] === undefined) {
+        this.connMap[id] = (firstId === null ? id : firstId);
+
+        if (this.unionMap[this.connMap[id]] === undefined) {
+          this.unionMap[this.connMap[id]] = [id];
+        } else {
+          this.unionMap[this.connMap[id]].push(id);
+        }
+      }
+
+      if (firstId === null) {
+        firstId = this.connMap[id];
+      }
+
+      if (this.connMap[id] !== firstId) {
+        const combineId = this.unionMap[firstId].length <= this.unionMap[this.connMap[id]].length ? firstId : this.connMap[id];
+        const combineToId = this.unionMap[firstId].length <= this.unionMap[this.connMap[id]].length ? this.connMap[id] : firstId;
+
+        this.unionMap[combineId].forEach(uid => {
+          this.connMap[uid] = combineToId;
+          this.unionMap[combineToId].push(uid);
+        });
+
+        delete this.unionMap[combineId];
+        firstId = combineToId;
+      }
+    });
   }
 
   /**
@@ -27,6 +61,8 @@ module.exports = class ConnectionDetector {
    * @param {any} nodeB
    */
   isConntectedTo(nodeA, nodeB) {
-
+    const idA = this.options.identify(nodeA);
+    const idB = this.options.identify(nodeB);
+    return this.connMap[idA] !== undefined && this.connMap[idB] !== undefined && this.connMap[idA] === this.connMap[idB];
   }
 }
