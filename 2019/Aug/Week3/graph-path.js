1719e25b6fa63b0f94bc6b13039a05e2fe9347cb
kongxiangjin <1351882069@qq.com>
2019-08-21 23:45:24 +0800

    2019-aug-week3

--- 2019/Aug/Week3/graph-path.js
+++ 2019/Aug/Week3/graph-path.js
@@ -7,14 +7,55 @@ class Graph {
    * @param {Array<[node1, node2]>} nodePairs 初始化的连通节点对
    */
   constructor(nodePairs) {
+    this.Adjacency = [];
+    this.graphMap = new Map();
+    // eslint-disable-next-line prefer-spread
+    this.nodes = [...new Set([].concat.apply([],nodePairs))];
+    this.init(this.nodes);
+    this.edge = [];
+    this.adjEdge = [];
 
+    for (let i = 0; i < nodePairs.length; i++) {
+      this.edge.push(nodePairs[i]);
+      this.addEdge(nodePairs[i][0], nodePairs[i][1])
+    }
+    this.createAdjacency(this.Adjacency);
   }
+
+
+  init(nodes) {
+    // eslint-disable-next-line guard-for-in,no-restricted-syntax
+    for(const item in nodes) {
+      this.Adjacency[nodes[item]] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.Adjacency[v1].push(v2);
+    this.Adjacency[v2].push(v1);
+  };
+
+  createAdjacency(Adjacency) {
+    // eslint-disable-next-line guard-for-in,no-restricted-syntax
+    for(const item in Adjacency) {
+      this.graphMap.set(parseInt(item, 10),this.Adjacency[item] )
+    }
+  };
+
 
   /**
    * @param {[node1, node2]} nodePair 连通的节点对
    */
   addNodePair(nodePair) {
-
+    this.nodes = [...new Set(nodePair.concat([...this.nodes]))];
+    this.init(this.nodes);
+    this.edge.push(nodePair);
+    for (let i = 0; i < this.edge.length; i++) {
+      this.addEdge(this.edge[i][0], this.edge[i][1])
+      // eslint-disable-next-line prefer-destructuring
+      this.adjEdge[this.edge[i][1]] = this.edge[i][0];
+    }
+    this.createAdjacency(this.Adjacency);
   }
 
   /**
@@ -23,7 +64,8 @@ class Graph {
    * @return {Paths}
    */
   search(source) {
-
+    // eslint-disable-next-line no-use-before-define
+    return new Paths(this, source);
   }
 }
 
@@ -38,7 +80,18 @@ class Paths {
    * @param {any} source
    */
   constructor(graph, source) {
+    this.map = graph.graphMap;
+    this.nodes = graph.nodes;
+    this.searchNode = source;
+    this.vertices = [];
+    this.marked = [];
+    this.edge = graph.adjEdge;
 
+    for(let i=0; i<this.nodes.length; i++){
+      this.marked[this.nodes[i]] = false;
+    }
+
+    this.dfs(source);
   }
 
   /**
@@ -47,11 +100,43 @@ class Paths {
    * @returns {boolean}
    */
   hasPathTo(target) {
-
+    for (let i = 0; i < this.vertices.length; i++) {
+      if (this.vertices[i] === target) {
+        return true;
+      }
+    }
+    return false
   }
 
-  pathTo(target) {
 
+  dfs(v) {
+    this.marked[v] = true;
+    if(this.map.get(v)!==undefined){
+      this.vertices.push(v)
+    }
+    for(let i=0; i < this.map.get(v).length; i++) {
+      const values = this.map.get(v);
+      for(let j=0; j < values.length; j++ ){
+        if(!this.marked[values[j]]) {
+          this.dfs(values[j])
+        }
+      }
+    }
+  }
+
+
+
+  pathTo(target) {
+    const path = [];
+    for (let i = target; i !== this.searchNode; i = this.edge[i]) {
+      path.push(i);
+      if (this.edge[i] === undefined) {
+        return null;
+
+      }
+    }
+    path.push(this.searchNode);
+    return path.reverse()
   }
 }
 
@@ -59,3 +144,4 @@ module.exports = {
   Graph,
   Paths,
 };
+
